a8ef84d585954dbc5c9e0c0896d6128069d198eb
Adam Markowski <gobolabxd@gmail.com>
2024-09-08 15:57:18 +0200

    aha

---
+++ app/components/name.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Name = () => {
+  return (
+    <div className='custom font-bold bg-slate-300 w-fit'>
+        <h2 >Adam</h2>
+        <h1>Markowski</h1>
+
+        <div className='flex justify-between mt-9'>
+        <span>Artist/Animator</span>
+        <span>Designer/WebDev</span>
+        </div>
+
+    </div>
+  )
+}
+
+export default Name
